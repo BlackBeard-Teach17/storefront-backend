05e2cf03e7e167d4cba889eb8cb2fa4e2d0a8af0
keamo <keamogetswemfoloe@gmail.com>
2023-01-11 22:07:08 +0200

    Feat:Create DB config for dev and test envs

---
+++ src/configs/database.config.ts
@@ -0,0 +1,37 @@
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
+
+dotenv.config();
+
+const {
+    POSTGRES_HOST,
+    POSTGRES_PORT,
+    POSTGRES_USER,
+    POSTGRES_PASSWORD,
+    POSTGRES_DB,
+    ENV
+} = process.env;
+
+let client;
+
+if (ENV == 'Dev'){
+    client = new Pool({
+        host: POSTGRES_HOST,
+        port: Number(POSTGRES_PORT),
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD,
+        database: POSTGRES_DB
+    });
+}
+
+if (ENV == 'Test'){
+    client = new Pool ({
+        host: POSTGRES_HOST,
+        port: Number(POSTGRES_PORT),
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD,
+        database: POSTGRES_DB
+    })
+}
+
+export default client;
