8c28e91ff4458d8d383354cee26e74a29b4f2cd8
keamo <keamogetswemfoloe@gmail.com>
2023-01-12 18:05:31 +0200

    Feat:Add more product models and routes

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -34,9 +34,82 @@ const create = async (_req: Request, res: Response) => {
     }
 }
 
+const show = async (_req: Request, res: Response) => {
+    try{
+        const showProduct = await store.show(_req.params.id);
+        res.json(showProduct);
+    } catch(err)
+    {
+        throw new Error(`Could not find product ${_req.params.id}. Error: ${err}`);
+    }
+}
+const updateAll = async (_req: Request, res: Response) => {
+    const product: Product = {
+        id: parseInt(_req.params.id),
+        name: _req.body.name,
+        price: _req.body.price,
+        category: _req.body.category
+    }
+
+    try{
+        const updatedProduct = await store.updateAll(product);
+        res.json(updatedProduct);
+    } catch(err) {
+        res.status(400);
+        res.json(err);
+    }
+}
+
+const updateName = async (_req: Request, res: Response) => {
+
+    try{
+        const updatedProduct = await store.updateProductName(_req.params.id, _req.body.name);
+        res.json(updatedProduct);
+    } catch(err) {
+        res.status(400);
+        res.json(err);
+    }
+}
+
+const updatePrice = async (_req: Request, res: Response) => {
+    try{
+        const updatedProduct = await store.updateProductPrice(_req.params.id, _req.body.price);
+        res.json(updatedProduct);
+    } catch(err)
+    {
+        throw new Error(`Could not update product ${_req.params.id}. Error: ${err}`);
+    }
+}
+
+const updateProductCategory = async (_req: Request, res: Response) => {
+    try{
+        const updatedProduct = await store.updateProductCategory(_req.params.id, _req.body.category);
+        res.json(updatedProduct);
+    } catch(err)
+    {
+        throw new Error(`Could not update product ${_req.params.id}. Error: ${err}`);
+    }
+}
+
+const destroy = async (_req: Request, res: Response) => {
+    try{
+        const deletedProduct = await store.delete(_req.params.id);
+        res.json(deletedProduct);
+    } catch(err) {
+        res.status(400);
+        res.json(err);
+    }
+}
+
 const product_routes = (app: express.Application) => {
-    app.get('/products', index);
+    app.get('/products/', index);
     app.post('/products/create', create);
+    app.get('/products/:id', show);
+    app.put('/products/update/:id', updateAll);
+    app.put('/products/update/:id/name', updateName);
+    app.put('/products/update/:id/price', updatePrice);
+    app.put('/products/update/:id/category', updateProductCategory);
+    app.delete('/products/:id', destroy);
 }
 
 export default product_routes;
--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -11,7 +11,6 @@ export class ProductStore {
     async index(): Promise<Product[]> {
         try {
             const conn = await client.connect();
-            console.log("Connecting to database");
             const sql = 'SELECT * FROM products';
             const result = await conn.query(sql);
             conn.release();
@@ -21,9 +20,28 @@ export class ProductStore {
         }
     }
 
-    async create(p: Product): Promise<Product> {
+    async isDuplicate(name: string): Promise<boolean> {
+        try {
+            const conn = await client.connect();
+            const sql = 'SELECT * FROM products WHERE name=($1)';
+            const result = await conn.query(sql, [name]);
+            conn.release();
+            if (result.rows.length > 0)
+                return true;
+            else
+                return false;
+        } catch (err) {
+            throw new Error(`Could not find product ${name}. Error: ${err}`);
+        }
+    }
+
+    async create(p: Product): Promise<Product | string> {
         try{
-            console.log("Creating product" + p.name + " " + p.price + " " + p.category);
+            if (await this.isDuplicate(p.name))
+            {
+                const duplicate_product = `Product ${p.name} already exists`;
+                return duplicate_product;
+            }
             const conn = await client.connect();
             const sql = 'INSERT INTO products (name, price, category) VALUES($1, $2, $3)';
             const result = await conn.query(sql, [p.name, p.price, p.category]);
@@ -33,6 +51,79 @@ export class ProductStore {
         }catch (err)
         {
             throw new Error(`Could not add new product ${p.name}. Error: ${err}`);
+        }
+    }
+
+    async show(id: string): Promise<Product> {
+        try {
+            const conn = await client.connect();
+            const sql = 'SELECT * FROM products WHERE id=($1)';
+            console.log("Product found");
+            const result = await conn.query(sql, [id]);
+            conn.release();
+            return result.rows[0];
+        } catch (err) {
+            throw new Error(`Could not find product ${id}. Error: ${err}`);
+        }
+    }
+
+    async delete(id: string): Promise<Product> {
+        try {
+            const conn = await client.connect();
+            const sql = 'DELETE FROM products WHERE id=($1)';
+            const result = await conn.query(sql, [id]);
+            conn.release();
+            return result.rows[0];
+        } catch (err) {
+            throw new Error(`Could not delete product ${id}. Error: ${err}`);
+        }
+    }
+
+    async updateAll(p: Product): Promise<Product> {
+        try {
+            const conn = await client.connect();
+            const sql = 'UPDATE products SET name=($1), price=($2), category=($3) WHERE id=($4)';
+            const result = await conn.query(sql, [p.name, p.price, p.category, p.id]);
+            conn.release();
+            return result.rows[0];
+        } catch (err) {
+            throw new Error(`Could not update product ${p.id}. Error: ${err}`);
+        }
+    }
+
+    async updateProductName(id: string, name: string): Promise<Product> {
+        try {
+            const conn = await client.connect();
+            const sql = 'UPDATE products SET name=($1) WHERE id=($2)';
+            const result = await conn.query(sql, [name, id]);
+            conn.release();
+            return result.rows[0];
+        } catch (err) {
+            throw new Error(`Could not update product ${id}. Error: ${err}`);
+        }
+    }
+
+    async updateProductPrice(id: string, price: number): Promise<Product> {
+        try{
+            const conn = await client.connect();
+            const sql = 'UPDATE products SET price=($1) WHERE id=($2)';
+            const result = await conn.query(sql, [price, id]);
+            conn.release();
+            return result.rows[0];
+        } catch (err) {
+            throw new Error(`Could not update product ${id}. Error: ${err}`);
+        }
+    }
+
+    async updateProductCategory(id: string, category: string): Promise<Product> {
+        try{
+            const conn = await client.connect();
+            const sql = 'UPDATE products SET category=($1) WHERE id=($2)';
+            const result = await conn.query(sql, [category, id]);
+            conn.release();
+            return result.rows[0];
+        } catch (err) {
+            throw new Error(`Could not update product ${id}. Error: ${err}`);
         }
     }
 }
