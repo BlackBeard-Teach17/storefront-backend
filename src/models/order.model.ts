a750c94595de8b28e874edc71f5fe3ca0f842b80
keamo <keamogetswemfoloe@gmail.com>
2023-01-17 15:24:35 +0200

    Added getCart item and fixed some of the models

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -34,8 +34,8 @@ export class OrderStore {
     async create(o: Order): Promise<Order> {
         try {
             const conn = await client.connect();
-            const sql = 'INSERT INTO orders (user_id, status) VALUES($1, $2)';
-            const result = await conn.query(sql, [o.user_id, o.status]);
+            const sql = 'INSERT INTO orders (status, user_id) VALUES($1, $2)';
+            const result = await conn.query(sql, [o.status, o.user_id]);
             conn.release();
             return result.rows[0];
         } catch (err) {
@@ -43,10 +43,11 @@ export class OrderStore {
         }
     }
 
+    // This function is used to check if the order is completed or not.
     async checkOrderStatus(order_id: string): Promise<Boolean> {
         try {
             const conn = await client.connect();
-            const sql = 'SELECT status FROM orders WHERE order_id=($1)';
+            const sql = 'SELECT status FROM orders WHERE id=($1)';
             const result = await conn.query(sql, [order_id]);
             conn.release();
             if(result.rows[0].status !== 'completed'){
@@ -59,15 +60,18 @@ export class OrderStore {
     }
 
     // This function is used to check if the product is already in the order and update the quantity if it is.
-    async checkOrderProductExists(order_id: string, product_id: string, quantity: number): Promise<Boolean> {
+    async checkOrderProductExists(order_id: string, product_id: number, quantity: number): Promise<Boolean> {
         try {
             const conn = await client.connect();
-            const sql = 'SELECT product_id, quantity FROM order_products WHERE order_id=($1)';
-            const result = await conn.query(sql, [order_id]);
+            const sql = 'SELECT product_id, quantity FROM order_products WHERE order_id=($1) AND product_id=($2)';
+            const result = await conn.query(sql, [order_id, product_id]);
             conn.release();
+            if (result.rows[0] === undefined){
+                return false;
+            }
             if(result.rows[0].product_id === product_id){
                 const new_quantity = result.rows[0].quantity + quantity;
-                await this.updateOrderProduct(parseInt(order_id), parseInt(product_id), new_quantity);
+                await this.updateOrderProduct(parseInt(order_id), product_id, new_quantity);
                 return true;
             }
             return false;
@@ -89,14 +93,13 @@ export class OrderStore {
         }
     }
 
-    async addToCart(order_id: number, product_id: number, quantity: number): Promise<Order | string> {
+    async addToCart(order_id: string, product_id: number, quantity: number): Promise<Order | string> {
         if (await this.checkIfOrderExists(order_id.toString()) === false)
             return 'Order does not exist';
         if (await this.checkOrderStatus(order_id.toString()) === false)
             return 'Order is completed';
-        if (await this.checkOrderProductExists(order_id.toString(), product_id.toString(), quantity) === true)
+        if (await this.checkOrderProductExists(order_id, product_id, quantity) === true)
             return 'Order updated';
-
         return await this.addProduct(order_id, product_id, quantity);
     }
 
@@ -115,7 +118,7 @@ export class OrderStore {
         }
     }
 
-    async addProduct(order_id: number, product_id: number, quantity: number): Promise<Order | string> {
+    async addProduct(order_id: string, product_id: number, quantity: number): Promise<Order | string> {
         try {
             const conn = await client.connect();
             const sql = 'INSERT INTO order_products (order_id, product_id, quantity) VALUES($1, $2, $3)';
@@ -127,9 +130,28 @@ export class OrderStore {
         }
     }
 
-    // This function is used to remove a product from an order.
-    async removeProduct(order_id: number, product_id: number): Promise<Order | string> {
+    async getCartItems(order_id: number): Promise<Order | string> {
+        if (await this.checkIfOrderExists(order_id.toString()) === false)
+            return 'Order does not exist';
         if (await this.checkOrderStatus(order_id.toString()) === false)
+            return 'Order is completed';
+        try {
+            const conn = await client.connect();
+            const sql = 'SELECT p.name as product_name, p.price, op.quantity, p.price * op.quantity as total \
+            FROM order_products op \
+            JOIN products p ON op.product_id = p.id \
+            WHERE op.order_id = ($1)';
+            const result = await conn.query(sql, [order_id]);
+            conn.release();
+            return result.rows;
+        } catch (err) {
+            throw new Error(`Could not find order ${order_id}. Error: ${err}`);
+        }
+    }
+    // This function is used to remove a product from an order.
+    async removeProduct(order_id: string, product_id: number): Promise<Order | string> {
+        console.log('removeProduct: ' + order_id + ' ' + product_id)
+        if (await this.checkOrderStatus(order_id) === false)
             return 'Order is completed';
         try {
             const conn = await client.connect();
